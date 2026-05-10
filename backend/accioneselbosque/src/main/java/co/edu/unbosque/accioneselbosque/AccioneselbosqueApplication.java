704769cd644dfde1916537798f57226983aaed7e
Julian Camilo Diaz Osorno <134356840+jul1annt@users.noreply.github.com>
2026-05-09 22:30:30 -0500

    Add New Spring Proyect

---
+++ backend/accioneselbosque/src/main/java/co/edu/unbosque/accioneselbosque/AccioneselbosqueApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.accioneselbosque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccioneselbosqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccioneselbosqueApplication.class, args);
+	}
+
+}
---
+++ backend/accioneselbosque/src/test/java/co/edu/unbosque/accioneselbosque/AccioneselbosqueApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.accioneselbosque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccioneselbosqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
